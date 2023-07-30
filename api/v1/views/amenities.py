27baf88383717d979ee7aed50ea406a56368beb9
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-07-30 14:07:40 +0200

    task 8, 9

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Handles REST operations for amentites object"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from flask import abort
+from flask import request
+from flask import make_response
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+
+@app_views.route("/amenities", strict_slashes=False, methods=["GET", "POST"])
+def amenity():
+    if request.method == "GET":
+        return jsonify([amenity.to_dict()
+                       for amenity in storage.all(Amenity).values()])
+    else:
+        new_dict = request.get_json()
+        if new_dict:
+            if new_dict.get("name"):
+                amenity = Amenity(**new_dict)
+                amenity.save()
+                return make_response(jsonify(amenity.to_dict()), 201)
+            else:
+                return make_response({"error": "Missing name"}, 400)
+        else:
+            return make_response({"error": "Not a JSON"}, 400)
+
+
+@app_views.route("/amenities/<amentiy_id>", strict_slashes=False,
+                 methods=["GET", "DELETE", "PUT"])
+def amentiy(amentiy_id):
+    amentiy = storage.get(Amenity, amentiy_id)
+    if amentiy:
+        if request.method == "GET":
+            return jsonify(amentiy.to_dict())
+        elif request.method == "DELETE":
+            amentiy.delete()
+            storage.save()
+            return jsonify({})
+        else:
+            new_dict = request.get_json()
+            if new_dict:
+                for k, v in new_dict.items():
+                    if k not in ['id', 'created_at', 'updated_at']:
+                        setattr(amentiy, k, v)  # risk
+                amentiy.save()
+                return jsonify(amentiy.to_dict())
+            else:
+                return make_response({"error": "Not a JSON"}, 400)
+    abort(404)
---
+++ api/v1/views/users.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Handles REST operations for amentites object"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from flask import abort
+from flask import request
+from flask import make_response
+from models import storage
+from models.user import User
+
+
+@app_views.route("/users", strict_slashes=False, methods=["GET", "POST"])
+def get_users():
+    if request.method == "GET":
+        return jsonify([user.to_dict()
+                       for user in storage.all(User).values()])
+    else:
+        new_dict = request.get_json()
+        if new_dict:
+            if new_dict.get("name"):
+                user = User(**new_dict)
+                user.save()
+                return make_response(jsonify(user.to_dict()), 201)
+            else:
+                return make_response({"error": "Missing name"}, 400)
+        else:
+            return make_response({"error": "Not a JSON"}, 400)
+
+
+@app_views.route("/users/<user_id>", strict_slashes=False,
+                 methods=["GET", "DELETE", "PUT"])
+def handel_user(user_id):
+    user = storage.get(User, user_id)
+    if user:
+        if request.method == "GET":
+            return jsonify(user.to_dict())
+        elif request.method == "DELETE":
+            user.delete()
+            storage.save()
+            return jsonify({})
+        else:
+            new_dict = request.get_json()
+            if new_dict:
+                for k, v in new_dict.items():
+                    if k not in ['id', 'created_at', 'updated_at']:
+                        setattr(user, k, v)  # risk
+                user.save()
+                return jsonify(user.to_dict())
+            else:
+                return make_response({"error": "Not a JSON"}, 400)
+    abort(404)
